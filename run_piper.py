afba22d87b03f49b670639fa8cb4da1c42e9c61a
“Ezequiel <ezequielamin@outlook.com>
2026-01-15 15:55:59 -0300

    feat: create pipecat project

---
+++ run_piper.py
@@ -0,0 +1,50 @@
+import os
+import subprocess
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+def main():
+    # ─────────────────────────────────────────────
+    # Cargar .env desde la carpeta padre
+    # ─────────────────────────────────────────────
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    load_dotenv(".env")
+
+    voice_model = os.getenv("CURRENT_VOICE")
+    voice_config = os.getenv("CURRENT_VOICE_CONFIG")
+    port = os.getenv("PIPER_PORT", "5002")
+
+    if not voice_model or not voice_config:
+        raise RuntimeError(
+            "Faltan variables en el .env: CURRENT_VOICE y/o CURRENT_VOICE_CONFIG"
+        )
+
+    voice_model = "voice/" + voice_model
+    voice_config = "voice/" + voice_config
+
+    print("Usando modelo de voz:", voice_model)
+    print("Usando config de voz:", voice_config)
+
+    cmd = [
+        "uv",
+        "run",
+        "python3",
+        "-m",
+        "piper.http_server",
+        "--model",
+        str(voice_model),
+        "--data-dir",
+        str(voice_config),
+        "--port",
+        str(port),
+    ]
+
+    print("🚀 Levantando Piper TTS")
+    print("▶", " ".join(cmd))
+
+    subprocess.run(cmd, check=True)
+
+
+if __name__ == "__main__":
+    main()
