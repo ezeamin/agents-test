1232fa6a125d50849503c2c204e9f42d949047ec
Juan Manuel López <lopezjuanma96@gmail.com>
2026-01-26 16:15:26 +0000

    added deepgram (tested) and openai for vllm (could not test it).\nThis are alternatives to Whisper STT but they have streaming.

--- src/helpers/services.py
+++ src/helpers/services.py
@@ -3,7 +3,10 @@ import os
 import ssl
 import aiohttp
 
+from deepgram import LiveOptions
 from pipecat.services.whisper.stt import WhisperSTTService
+from pipecat.services.openai.stt import OpenAISTTService
+from pipecat.services.deepgram.stt import DeepgramSTTService
 from pipecat.services.aws.llm import AWSBedrockLLMService
 from pipecat.services.piper.tts import PiperTTSService
 
@@ -28,12 +31,31 @@ except ImportError:
 
 def create_stt_service():
     """Crea y configura el servicio de Speech-to-Text (Whisper)"""
-    return WhisperSTTService(
-        model="medium",
-        device="cpu",
-        compute_type="int8", 
-        language="es",
-    )
+    if False:
+        return OpenAISTTService(
+            model="mistralai/Voxtral-Mini-3B-2507",
+            base_url="http://localhost:8000/v1",
+            api_key="NONE"
+        )
+    elif False:
+        return WhisperSTTService(
+            model="medium",
+            device="cpu",
+            compute_type="int8", 
+            language="es",
+        )
+    elif True:
+        live_options = LiveOptions(
+            model="nova-3",
+            language="es-419",
+            interim_results=True,
+            smart_format=True,
+            punctuate=True
+        )
+        return DeepgramSTTService(
+            live_options=live_options,
+            api_key=os.getenv("DEEPGRAM_API_KEY")
+        )
 
 
 def create_tts_service(session: aiohttp.ClientSession):
